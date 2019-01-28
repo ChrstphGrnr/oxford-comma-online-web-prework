05c877ffc870a9be4fa2c3ce142305ce272cabeb
Christoph Gruener <christophgruener@me.com>
2019-01-28 19:19:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,13 +5,8 @@ def oxford_comma(array)
    array.insert(-2, " and ")
    array.join
  elsif array.length > 2 
-   array_length = array.length-1
-   while array_length > 0 
-    array.insert(array_length, ", ")
-    array_length -= 1 
-  end
-  array.insert(-2, "and ")
-  array.join
+   array.insert(-2, "and ")
+   array.join(", ")
 else
 end
 end
